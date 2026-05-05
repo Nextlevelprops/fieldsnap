b8377c8a490e80e754e83e5302134c21fe62236f
Nextlevelprops <info@nextlevelprops.com>
2026-05-05 09:27:14 -0500

    upgrade supabase client and use publishable key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = "https://aheyxqzdvyctghcuqjlp.supabase.co"
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFoZXl4cXpkdnljdGdoY3VxamxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2MTY4NDgsImV4cCI6MjA5MzE5Mjg0OH0.n2a6TcsMVB6xeFOFhCg_Mnz1VuKAwQChC6mvffjTLJwM"
+const SUPABASE_ANON_KEY = "sb_publishable_yEV9axXvyvLEwzhcalh0vg_BcSo5MUx"
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
